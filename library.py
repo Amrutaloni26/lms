38c5e4536667a33be9e8c8c33ee19d73ffdeefe5
Amruta <amrutaloni26@gmail.com>
2021-12-25 16:16:49 +0530

    added space for title

--- library.py
+++ library.py
@@ -5,8 +5,9 @@ from tkinter import ttk
 class LibraryManagementSystem:
     def __init__(self,root):
         self.root=root
-        self.root.title("Library Management System")    
+        self.root.title("Library Management System ")    
         self.root.geometry("1550x800+0+0")
+        
 
 
         lbltitle=Label(self.root,text="LIBRARY MANAGEMENT SYSTEM",bg="powder blue",fg="green",bd=16,relief=RIDGE,font=("times new roman",30,"bold"),padx=2,pady=6)
